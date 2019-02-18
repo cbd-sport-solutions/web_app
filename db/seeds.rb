cdf6c3c248734829ebcbd3aa4c286f628db873d8
Luke Chambers <lukenatchambers@gmail.com>
2019-02-18 10:58:04 -0700

    Integrate products into home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i = 0
+start_time = Time.now
+p "Seeding products"
+10.times do
+  Product.create!(name: "Product #{i}",
+    price: 29.99,
+    description: "This is a long description about how this is going to take up too much room and needs to be shortened #{i}",
+    image: "https://picsum.photos/200/300/?random",
+    score: rand(0.0..5.0).round(2)
+  )
+  i += 1
+end
+end_time = Time.now
+p "Products seeded; total time was #{start_time - end_time}"

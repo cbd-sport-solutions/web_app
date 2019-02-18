3d8c70bd7237b42c81bd9ed301f60bfd49adf4cf
Luke Chambers <lukenatchambers@gmail.com>
2019-02-18 15:58:16 -0700

    Remove auth checks temporarily

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,6 +2,6 @@ class BaseController < ApplicationController
 
   def check_auth
     @user = User.find(params[:id])
-    render :404 unless User.admin?
+    # render :404 unless User.admin?
   end
 end

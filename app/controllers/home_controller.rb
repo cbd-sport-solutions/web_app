e31eed45f4145315b75c8bedf75452c39b4763a7
Luke Chambers <lukenatchambers@gmail.com>
2019-03-11 14:53:31 -0600

    Pivot to landing pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,7 @@ class HomeController < ApplicationController
 
   def index
     redirect_to :controller => 'coming', :action => 'index'
-    @products = Product.where(active: true)
-    @featured_products = []
-    3.times do
-      @featured_products << @products.shuffle.pop
-    end
+    @products = Product.all.order(active: :desc)
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,5 @@
 class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all.order(active: :desc)
-  end
-
   def show
     @product = Product.find(params[:id])
   end

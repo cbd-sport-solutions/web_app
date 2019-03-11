b7946254ff9666681f405ee13015cd3d35f366c4
Luke Chambers <lukenatchambers@gmail.com>
2019-03-11 14:40:39 -0600

    Pivot

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :require_admin
 
   def require_admin
-    render file: "/public/404" unless current_admin?
+    # render file: "/public/404" unless current_admin?
   end
 
 end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < AdminController
 
   def index
-
+    @orders = Orders.all.order(status: :desc)
+    @products = Product.all.order(active: :desc)
   end
+
 end
